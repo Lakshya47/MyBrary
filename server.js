ffd1a58c883df6cac44f920523994b446d2ed087
Lakshya Chaudhary <chaudharylakshya2@gmail.com>
2021-10-22 18:58:18 +0530

    initial author model setup

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded())
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,8 +26,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT || 4000)
-
-
-/* Resume from 12:00 minutes */
+app.listen(process.env.PORT || 5000)
